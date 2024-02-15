682ec7e933060022ca29042ee81368d5a151a6aa
Cassie-Ch-0909 <158829893+Cassie-Ch-0909@users.noreply.github.com>
2024-02-15 23:05:39 +0800

    轮播图模块写注释

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BoxReflect from '@/pages/Home/components/BoxReflect.vue'
 
+// 定义一个轮播图数组
 const carouselList = ref([
   'https://img2023.gcsis.cn/2023/4/3dd1997060ca4d8f95dc01e1e8be8b48.jpg',
   'https://img2023.gcsis.cn/2023/7/cf17e6301f9d4035ab0a3066333d5727.jpg',
@@ -11,15 +12,20 @@ const carouselList = ref([
 
 <template>
   <el-carousel height="780px" class="h-[200px] w-full md:h-[780px]">
+    <!-- 轮播图的每一项 -->
     <el-carousel-item
       v-for="(item, index) in carouselList" :key="index" class="h-[200px] w-full md:h-[780px]"
       :class="index === 0 ? 'relative' : ''"
     >
+      <!-- 数字安全@数字中国 -->
       <img
-        src="@/assets/el-carouse/2.webp" alt="" class="animate__animated animate__flipInX animate__slow absolute left-22% top-20% w-60%"
+        src="@/assets/el-carouse/2.webp" alt=""
+        class="animate__animated animate__flipInX animate__slow absolute left-22% top-20% w-60%"
         :class="index !== 0 ? 'hidden' : ''"
       >
+      <!-- 每一张图片 -->
       <img :src="item" alt="" class="h-[200px] w-full md:h-[780px]">
+      <!-- button -->
       <BoxReflect
         class="animate__animated animate__flipInX animate__delay-1s absolute md:left-5% md:top-30%"
         :class="index !== 0 ? 'hidden' : ''"
