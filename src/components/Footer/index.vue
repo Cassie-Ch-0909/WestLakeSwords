246f9c7df5eb0665c89e327c49ab2d77bbe62ff6
Cassie-Ch-0909 <158829893+Cassie-Ch-0909@users.noreply.github.com>
2024-02-25 15:42:44 +0800

    移动端底部Footer完成

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <!-- PC端底部Footer完成 -->
+  <!-- PC端底部Footer -->
   <div class="mt[50px] h-[550px] w-full from-[#169E9C] to-[#036CAD] bg-gradient-to-t max-md:hidden">
     <div class="ml10% mr10% h-full w80% color-[#fff]">
       <!-- 第一行 -->
@@ -93,7 +93,9 @@
             光明网
           </p>
         </div>
-        <div class="ml13% mt-[30px] h[165px] w-[150px] flex flex-col items-center justify-center border-[1px] border-[#fff] border-solid">
+        <div
+          class="ml13% mt-[30px] h[165px] w-[150px] flex flex-col items-center justify-center border-[1px] border-[#fff] border-solid"
+        >
           <img src="/public/footer/1.webp" class="w-[130px]">
           <p>扫码进入小程序</p>
         </div>
@@ -105,6 +107,108 @@
       <div class="h-[80px] w-full flex items-center justify-center font-size-[12px]">
         © 杭州安恒信息技术股份有限公司 浙ICP备09102757号-28 浙公网安备 33010802009170号
       </div>
+    </div>
+  </div>
+  <!-- 移动端底部Footer -->
+  <div class="mt-[20px] w-full from-[#169E9C] to-[#036CAD] bg-gradient-to-r pl5% pr3% pt3% color-[#fff] md:hidden">
+    <div class="h-[160px] w-full flex">
+      <div class="w-65%">
+        <p class="pb-[10px] font-size-[18px]">
+          指导单位
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省互联网信息办公室
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省经济和信息化厅
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省公安厅
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省发展和改革委员会
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省通信管理局
+        </p>
+      </div>
+      <div class="w-30%">
+        <p class="pb-[10px] font-size-[18px]">
+          主办单位
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          杭州市人民政府
+        </p>
+      </div>
+    </div>
+    <div class="mt5% h-[160px] w-full flex">
+      <div class="w-65%">
+        <p class="pb-[10px] font-size-[18px]">
+          联合主办单位
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          中国信息通信研究院
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          国家工业信息安全发展研究中心
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          中国电子技术标准化研究院
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          工业和信息化部教育与考试中心
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          杭州市滨江区人民政府
+        </p>
+      </div>
+      <div>
+        <div class="">
+          <p class="pb-[10px] font-size-[18px]">
+            战略合作媒体
+          </p>
+          <p class="pb-[2px] font-size-[12px]">
+            新华网
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="mt5% h-[160px] w-full flex">
+      <div class="w-65%">
+        <p class="pb-[10px] font-size-[18px]">
+          承办单位
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          杭州市互联网信息办公室
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          杭州市公安局
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          杭州市发展和改革委员会
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          浙江省网络安全空间安全协会
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          安恒信息
+        </p>
+      </div>
+      <div class="">
+        <p class="pb-[10px] font-size-[18px]">
+          特别支持媒体
+        </p>
+        <p class="pb-[2px] font-size-[12px]">
+          光明网
+        </p>
+      </div>
+    </div>
+    <div class="mt5% w-full flex flex-col items-center justify-center pb8% font-size-[12px]">
+      <p>隐私协议</p>
+      <p>服务条款</p>
+      <p>© 杭州安恒信息技术股份有限公司</p>
+      <p>浙ICP备09102757号-28</p>
+      <p>浙公网安备 33010802009170号</p>
     </div>
   </div>
 </template>
