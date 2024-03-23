d9335ad09fb1a775cd558b34947fad367f932331
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-03-23 22:56:29 +0800

    PC端我的订阅 我的点赞 我的收藏模型搭建完毕

--- src/pages/PersonalCenter/components/MyCollection.vue
+++ src/pages/PersonalCenter/components/MyCollection.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import MoudleHeader from './MoudleHeader.vue'
+import MyComponents from './MyComponents.vue'
+</script>
 
 <template>
-  <div>我的收藏</div>
+  <!-- 原始高度是h600px -->
+  <div class="mt10px w-full flex-1 bg-#fff shadow-lg">
+    <MoudleHeader title="我的收藏" />
+
+    <!-- 每个订阅的议程 -->
+    <MyComponents img="/public/activity/2.webp" title="西湖论剑网络特训营" type="掌上论剑" time="11:30-20:33" />
+  </div>
 </template>
 
 <style scoped></style>
