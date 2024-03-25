4ee8f6a8fe4fc92306da049c165fab34d589dfb6
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-03-25 12:50:51 +0800

    PC端限时领取西湖会员模块完成

--- src/pages/HotTopicDiscussion/components/Right.vue
+++ src/pages/HotTopicDiscussion/components/Right.vue
@@ -2,7 +2,18 @@
 </script>
 
 <template>
-  <div class="mt20px w-260px border-2px border-#E0E0E0 rounded-10px border-solid bg-#fff">
-    ghrufudhfieuhr
+  <div class="mt20px w-260px border-2px border-#E0E0E0 rounded-10px border-solid">
+    <!-- 限时领取西湖会员模块 -->
+    <div class="h90px w-full flex items-center justify-between rounded-10px bg-#fff p20px">
+      <div>
+        <p class="font-size-16px font-bold">
+          限时领取西湖会员
+        </p>
+        <p class="font-size-13px color-#999">
+          点亮在社区的每一天
+        </p>
+      </div>
+      <img class="w75px" src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/sign.d62085a.gif" alt="">
+    </div>
   </div>
 </template>
