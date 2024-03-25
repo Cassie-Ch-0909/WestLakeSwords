e169db12036529e728503986488310f73a0b4dba
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-03-25 13:52:17 +0800

    PC端作者排行榜完成

--- src/pages/HotTopicDiscussion/components/Right.vue
+++ src/pages/HotTopicDiscussion/components/Right.vue
@@ -38,7 +38,7 @@
         </ul>
       </div>
       <div class="h50px w-full flex items-center justify-center border-b-1 border-#E0E0E0 color-#999">
-        <span>查看更多</span>
+        <span class="font-size-13px">查看更多</span>
         <i class="iconfont icon-youjiantou ml5px font-size-12px" />
       </div>
     </div>
@@ -46,5 +46,52 @@
     <img src="/public/hottopic/2.jpg" class="mt15px" alt="">
     <img src="/public/hottopic/3.jpg" class="mt15px" alt="">
     <img src="/public/hottopic/1.jpg" class="mt15px" alt="">
+    <!-- 作者排行榜 -->
+    <div class="mt20px w-full flex flex-col bg-#fff">
+      <div class="h50px w-full flex items-center justify-between border-b-1 border-#E0E0E0 pl15px pr15px">
+        <div class="flex items-center">
+          <img class="w30px" src="/public/hottopic/article.png" alt="">
+          <span class="ml5px">文章榜</span>
+        </div>
+        <div class="flex items-center color-#999">
+          <i class="iconfont icon-huanyihuan mr5px font-size-15px" />
+          <p class="font-size-13px">
+            换一换
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex-1 border-b-1 border-#E0E0E0 font-size-14px">
+        <ul class="h-full w-full flex flex-col justify-evenly pb10px pt10px">
+          <li
+            v-for="(item, index) in 5" :key="index"
+            class="ml5% mr5% w-90% flex items-center justify-between p5px hover:bg-#E0E0E0"
+          >
+            <div class="h40px flex items-center font-size-12px">
+              <img
+                class="w-40px"
+                src="https://p3-passport.byteacctimg.com/img/user-avatar/06d48f05b22d366e72d9e10134610cd7~300x300.image"
+                alt=""
+              >
+              <div class="ml5px">
+                <p class="font-size-13px">
+                  京东云开发者
+                </p>
+                <p class="color-#999">
+                  技术运营 @京东科技信...
+                </p>
+              </div>
+            </div>
+            <div class="color-blue-400">
+              <span>+</span>
+              <span>关注</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="h50px w-full flex items-center justify-center border-b-1 border-#E0E0E0 color-#999">
+        <span class="font-size-13px">查看更多</span>
+        <i class="iconfont icon-youjiantou ml5px font-size-12px" />
+      </div>
+    </div>
   </div>
 </template>
