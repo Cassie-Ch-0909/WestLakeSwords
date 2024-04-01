5aa92be76275d9f693c312fb0594b97c2da89626
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-01 22:03:06 +0800

    好物商城积分订阅和兑换css美化与信息检验

--- src/pages/PersonalCenter/components/StoreGoods.vue
+++ src/pages/PersonalCenter/components/StoreGoods.vue
@@ -112,24 +112,24 @@ exchangeGoodsByIntegral(1)
 </script>
 
 <template>
-  <div class="mt20px w-full flex flex-wrap justify-evenly">
+  <div class="mt20px w-full flex flex-wrap">
     <div
       v-for="(item, index) in goodsList" :key="index"
-      class="mb20px w280px flex flex-col items-center bg-#fff shadow-xl"
+      class="mb20px ml2% w280px flex flex-col items-center bg-#fff shadow-xl"
     >
-      <img class="w-full" :src="item.img">
+      <img class="h220px w-full" :src="item.img">
       <p class="pb10px pt10px font-size-16px font-bold">
         {{ item.goodsName }}
       </p>
-      <div class="w-full flex font-size-15px color-[#00B4BC]">
-        <span class="ml50px font-bold">￥{{ item.price }}</span>
-        <button class="ml30px w100px rounded-10px bg-[#00B4BC] font-size-13px color-#fff" @click="showDialog(1, index)">
+      <div class="w-full flex justify-between pl50px pr50px font-size-15px color-[#00B4BC]">
+        <span class="font-bold">￥{{ item.price }}</span>
+        <button class="w100px rounded-10px bg-[#00B4BC] font-size-13px color-#fff" @click="showDialog(1, index)">
           去购买
         </button>
       </div>
-      <div class="mt10px w-full flex pb10px font-size-15px">
-        <span class="ml20px font-bold">兑换：{{ item.integral }}</span>
-        <button class="ml30px w100px rounded-10px bg-[#00B4BC] font-size-13px color-#fff" @click="showDialog(2, index)">
+      <div class="mt10px w-full flex justify-between pb10px pl20px pr50px font-size-15px">
+        <span class="font-bold">兑换：{{ item.integral }}</span>
+        <button class="w100px rounded-10px bg-[#00B4BC] font-size-13px color-#fff" @click="showDialog(2, index)">
           去兑换
         </button>
       </div>
@@ -143,12 +143,12 @@ exchangeGoodsByIntegral(1)
           {{ goodsList[activeGoodsIndex].goodsName }}
         </p>
         <p v-if="!buyOrExchangeFlag" class="mb10px mt15px flex items-center">
-          <span class="font-size-13px">价格：</span>
-          <span class="font-size-20px color-[#00B4BC] font-bold">￥{{ goodsList[activeGoodsIndex].price }}</span>
-        </p>
-        <p v-else class="mb10px mt15px flex items-center">
           <span class="font-size-13px">积分兑换：</span>
           <span class="font-size-20px color-[#00B4BC] font-bold">{{ goodsList[activeGoodsIndex].integral }}</span>
+        </p>
+        <p v-else class="mb10px mt15px flex items-center">
+          <span class="font-size-13px">价格：</span>
+          <span class="font-size-20px color-[#00B4BC] font-bold">￥{{ goodsList[activeGoodsIndex].price }}</span>
         </p>
         <p class="mt15px">
           {{ goodsList[activeGoodsIndex].goodsDesc }}
