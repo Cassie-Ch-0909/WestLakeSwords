812d69f95b0a4924de8d31d7635a08a3fadb8b62
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-02 21:25:29 +0800

    设置移动端可访问

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -8,7 +8,6 @@ declare global {
   const EffectScope: typeof import('vue')['EffectScope']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef']
-  const biSyncRef: typeof import('@vueuse/core')['biSyncRef']
   const computed: typeof import('vue')['computed']
   const computedAsync: typeof import('@vueuse/core')['computedAsync']
   const computedEager: typeof import('@vueuse/core')['computedEager']
